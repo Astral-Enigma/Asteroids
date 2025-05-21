0e628f3137e788007528923f6935fc9ed5e82e82
aster_ray <asterrae444@gmail.com>
2025-05-21 16:04:06 -0400

    Updates

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 '''
 GAME LOOP
@@ -7,6 +8,8 @@ GAME LOOP
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_HEIGHT,SCREEN_WIDTH))
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
@@ -19,6 +22,16 @@ def main():
             
         pygame.Surface.fill(screen,"black")
         pygame.display.flip()
+
+        clock.tick(60)
+        dt = clock.get_time()
+        dt = dt/1000
+
+        # Player.draw(screen)
+
+#Spawn player
+
+# Player(x=SCREEN_WIDTH/2,y=SCREEN_HEIGHT/2)
 
 '''
 End of file stuff
